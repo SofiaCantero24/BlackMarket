246bd5583ccecb78437495f5fea312a4ee83c5ed
Sofia Cantero <sofia.cantero@rootstrap.com>
2024-09-25 09:50:04 -0500

    feat: search bar component

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,29 +1,13 @@
 import images from 'assets';
 
-import { Image, Input, SafeAreaView, View } from '@/ui';
+import { SearchBar } from '@/components/search-bar';
+import { Image, SafeAreaView } from '@/ui';
 
 export default function Feed() {
   return (
     <SafeAreaView>
       <Image source={images.header()} className="h-14 w-full" />
-      <View className="w-full flex-row items-center bg-light_gray px-4 py-2">
-        <View className="h-12 w-full flex-row items-center justify-between rounded-lg border-[0.5px] border-neutral-400 bg-white pl-2 pr-8">
-          <View className="w-full flex-row items-center">
-            <Input
-              placeholder="Search for products"
-              className="-mb-2 p-2 text-black"
-              placeholderTextColor="#000000"
-            />
-          </View>
-          <View className="flex-row">
-            <Image
-              contentFit="contain"
-              source={images.search()}
-              className="mr-2 h-4 w-4"
-            />
-          </View>
-        </View>
-      </View>
+      <SearchBar />
     </SafeAreaView>
   );
 }
