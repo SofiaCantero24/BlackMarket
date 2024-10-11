2d9c50800e06282084445e134b31937db2182be4
Sofia Cantero <sofia.cantero@rootstrap.com>
2024-10-11 11:28:24 -0500

    fix: merge views on ProductCard

--- src/components/products-list/product-card.tsx
+++ src/components/products-list/product-card.tsx
@@ -14,49 +14,39 @@ export interface ProductCardProps {
 export const ProductCard = memo(
   ({ name, price, state, image_url }: ProductCardProps) => {
     return (
-      <View className="flex-row items-center justify-between rounded-lg bg-white p-3">
-        <View className="w-full flex-row items-center space-x-4">
-          <Image
-            source={{ uri: image_url }}
-            className="flex h-32 w-32"
-            contentFit="contain"
-          />
-          <View className="w-full flex-1 px-4">
-            <View className="flex-row items-center justify-between">
-              <View className="h-full w-3/4">
-                <Text className="text-xl font-bold">{name}</Text>
-              </View>
-              <View className="h-full">
-                <View className="h-12 w-12 rounded-full border border-black">
-                  <TouchableOpacity
-                    className="flex-auto rounded-full p-2"
-                    activeOpacity={0.7}
-                  >
-                    <Favorite
-                      color="#ffffff"
-                      stroke="#000000"
-                      strokeWidth={1}
-                      viewBox="-1 0 20 15"
-                    />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-            <View className="mb-2 mt-1 flex-row items-center space-x-2">
-              <View
-                className={`rounded-md rounded-tl-none px-2 py-1 ${
-                  state === 'used' ? 'bg-restored' : 'bg-new'
-                }`}
-              >
-                <Text className="text-xs font-semibold text-white">
-                  {state === 'used' ? 'Used' : 'New'}
-                </Text>
-              </View>
-            </View>
-            <View className="flex-row items-center justify-between">
-              <Text className="text-lg font-bold">{price}</Text>
-              <Button label="Add to cart" />
-            </View>
+      <View className="w-full flex-row items-center justify-between space-x-4 rounded-lg bg-white p-3">
+        <Image
+          source={{ uri: image_url }}
+          className="flex h-32 w-32"
+          contentFit="contain"
+        />
+        <View className="w-full flex-1 px-4">
+          <View className="flex-row items-center justify-between">
+            <Text className="h-full w-3/4 text-xl font-bold">{name}</Text>
+            <TouchableOpacity
+              className="h-12 w-12 rounded-full border border-black p-2"
+              activeOpacity={0.7}
+            >
+              <Favorite
+                color="#ffffff"
+                stroke="#000000"
+                strokeWidth={1}
+                viewBox="-2.5 -0.5 22 15"
+              />
+            </TouchableOpacity>
+          </View>
+          <View
+            className={`mb-2 mt-1 w-12 items-center space-x-2 rounded-md rounded-tl-none px-2 py-1 ${
+              state === 'used' ? 'bg-restored' : 'bg-new'
+            }`}
+          >
+            <Text className={`text-xs font-semibold text-white`}>
+              {state === 'used' ? 'Used' : 'New'}
+            </Text>
+          </View>
+          <View className="flex-row items-center justify-between">
+            <Text className="text-lg font-bold">{price}</Text>
+            <Button label="Add to cart" />
           </View>
         </View>
       </View>
