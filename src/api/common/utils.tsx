b8dd1689c69c8359483afd197cf1c3d4197cb279
Sofia Cantero <sofia.cantero@rootstrap.com>
2024-10-18 12:57:59 -0500

    feat: product detail hook

--- src/api/common/utils.tsx
+++ src/api/common/utils.tsx
@@ -77,3 +77,43 @@ export const toSnakeCase = (obj: GenericObject): GenericObject => {
   }
   return newObj;
 };
+
+const DEFAULT_ERROR_MESSAGE = 'something went wrong';
+const parseError = (data: any) => {
+  const { error, errors } = data || {};
+  if (error) {
+    return error;
+  }
+  if (errors) {
+    const { fullMessages, base } = errors;
+    if (fullMessages) {
+      const [firstMessage] = fullMessages;
+      return firstMessage;
+    } else if (base) {
+      const [firstMessage] = base;
+      return firstMessage;
+    } else if (Array.isArray(errors)) {
+      const [firstMessage] = errors;
+      return firstMessage;
+    } else {
+      const errorKey = Object.keys(errors)[0];
+      const error = errors[errorKey][0];
+      return `${errorKey} ${error}`;
+    }
+  }
+  return DEFAULT_ERROR_MESSAGE;
+};
+export const parseAxiosError = (error: any) => {
+  if (error) {
+    const { response } = error;
+    if (!response) {
+      return DEFAULT_ERROR_MESSAGE;
+    }
+    if (response.status === 500) {
+      return DEFAULT_ERROR_MESSAGE;
+    } else {
+      return parseError(response?.data);
+    }
+  }
+  return DEFAULT_ERROR_MESSAGE;
+};
---
+++ src/api/products/use-details.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { client, parseAxiosError } from '../common';
+import type { Product } from './types';
+export const getItemDetails = async (id: number): Promise<Product> => {
+  try {
+    const { data } = await client.get(`product/${id}`);
+    return data;
+  } catch (error) {
+    throw parseAxiosError(error);
+  }
+};
+export const useGetItemDetails = (id: number) =>
+  useQuery({
+    queryKey: ['getItemDetails'],
+    queryFn: () => getItemDetails(id),
+  });
