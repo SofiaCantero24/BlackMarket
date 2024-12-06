e26409375f15162aaaf31eb3ae8676c86c24dbc8
Sofia Cantero <sofia.cantero@rootstrap.com>
2024-12-06 13:47:03 -0500

    fix: componentize banner

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,36 +1,47 @@
 import images from 'assets';
 
 import { HeaderLogo } from '@/components/header-logo';
+import { Banner } from '@/components/home/bannner';
 import { HorizontalCarousel } from '@/components/home/carousel';
 import PaymentMethods from '@/components/home/payment-methods';
-import { Image, SafeAreaView, ScrollView, Text, View } from '@/ui';
+import { SafeAreaView, ScrollView, Text, View } from '@/ui';
 
 export default function Feed() {
   return (
     <SafeAreaView className="bg-light_background">
       <HeaderLogo />
-      <ScrollView>
+      <ScrollView className="mb-6">
         <HorizontalCarousel />
-        <View className="h-52">
-          <Image
-            className="w-full flex-1"
-            source={images.productsBanner()}
-            contentFit="contain"
-          />
-        </View>
+        <Banner
+          alignment="left"
+          background="dark"
+          image={images.furnitureBanner()}
+        >
+          <Text className="mb-2 font-semibold color-white">
+            Check out our new and restored furniture
+          </Text>
+          <Text className="color-white">
+            Shop today and get a 10% discount!
+          </Text>
+        </Banner>
         <View className="bg-white">
-          <Text className="my-8 self-center text-lg font-bold">
+          <Text className="mb-8 mt-4 self-center text-lg font-bold">
             Payment methods
           </Text>
           <PaymentMethods />
         </View>
-        <View className="h-48">
-          <Image
-            className="w-full flex-1"
-            source={images.fedexBanner()}
-            contentFit="contain"
-          />
-        </View>
+        <Banner
+          alignment="right"
+          background="dark"
+          image={images.fedexBanner()}
+        >
+          <Text className="mb-2 font-semibold color-white">
+            We upgraded our shipments many levels up
+          </Text>
+          <Text className="color-white">
+            Powered by <Text className="color-green-400">FedEx</Text>
+          </Text>
+        </Banner>
       </ScrollView>
     </SafeAreaView>
   );
